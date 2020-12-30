dcf67ccfcd5a25cd521bb245c431237391ad7b58
dhanushkamath <dhanush2897@gmail.com>
2020-12-30 23:13:56 +0530

    added Dockerfile and docker-compose.yml

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const { bookRouter } = require('./routes/bookRouter')
 
+// environment variables
 const PORT = process.env.PORT || 3000;
+const MONGO_CONTAINER_NAME = process.env.MONGO_CONTAINER_NAME || 'localhost';
 
 // create an express app
 const app = express();
 
 // add basic logging
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(morgan(':method :url :status :res[content-length] :remote-addr - :response-time ms'));
 
 // middleware to parse request
 app.use(express.json());
 
 // mongoDB connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/bookDB', {
+mongoose.connect(`mongodb://${MONGO_CONTAINER_NAME}:27017/bookDB`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 },(err) => {
